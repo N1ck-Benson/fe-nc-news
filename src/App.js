9c2e4af40aa5b97b0b34f8ef63b7ce7f3d8e7c5b
Nick Benson <nk.benson@hotmail.com>
2021-05-12 09:02:11 +0100

    change landing page to home and modify router

--- src/App.js
+++ src/App.js
@@ -9,20 +9,21 @@ const App = () => {
   return (
     <main className="app">
       <header>
-        <Link to="/home" className="header-link">
+        <Link to="/" className="header-link">
           <h1>NC News</h1>
         </Link>
         <nav>
-          <Link to="/home" className="header-link">
+          <Link to="/" className="header-link">
             Home
           </Link>
-          <Link to="/home/topics" className="header-link">
+          <Link to="/topics" className="header-link">
             Topics
           </Link>
         </nav>
       </header>
+      <Home />
       <Router>
-        <Home path="home/*" />
+        <Home path="/" />
         <ArticlePage path="articles/:article_id" />
       </Router>
     </main>
